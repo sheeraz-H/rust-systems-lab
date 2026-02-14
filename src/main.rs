092d49ada9ff0127b07837cb24f44d780e276d6f
sheeraz-H <your-github-xherazusain@gmail.com>
2026-02-14 01:22:22 -0800

    first rust-hello

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let is_true:bool = true;
+
+    if is_true {
+        println("The number is  even")
+    }else {
+        println("The given number is not even!")
+    }
+    println("Hello, sheeraz-osain!");
+}
